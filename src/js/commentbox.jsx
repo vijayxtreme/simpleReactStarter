bb9c28484994250c34170aea2a8290923cf5b6b0
Vijay Menon <vijaymenonx@gmail.com>
2016-08-06 23:37:32 -0700

    Complete tutorial, save for writing node router for POST ajax

--- src/js/commentbox.jsx
+++ src/js/commentbox.jsx
@@ -10,6 +10,7 @@ export default class CommentBox extends Component {
 		this.state = {
 			data:[]
 		}
+		this.handleCommentSubmit = this.handleCommentSubmit.bind(this)
 	}
 	loadCommentsFromServer(){
 		console.log(this.props)
@@ -25,20 +26,43 @@ export default class CommentBox extends Component {
 	     	}.bind(this)
 	     });
 	}
+	handleCommentSubmit(comment){
+		console.log('submit')
+		console.log(this.state)
+		let comments = this.state.data;
+		comment.id = Date.now();
+		let newComments = comments.concat([comment]);
+		this.setState({data:newComments});
+
+		//TODO: submit to the server and refresh the list
+		// $.ajax({
+		// 	url:this.props.url,
+		// 	dataType:'json',
+		// 	data:comment,
+		// 	type:'POST',
+		// 	success:function(data){
+		// 		this.setState({data:data});
+		// 	}.bind(this),
+		// 	error:function(xhr, status, err){
+		// 		this.setState({data:comments});
+		// 		console.error(this.props.url, status, err.toString())
+		// 	}.bind(this)
+		// });
+	}
 	componentDidMount() {
 		this.loadCommentsFromServer();
 		//setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval);
 		// OR... use a fat arrow, which preserves `this` context
 		//setInterval(() => this.loadCommentsFromServer(), this.props.pollInterval); 
 
-		setInterval(() => this.loadCommentsFromServer(), this.props.pollInterval)
+		//setInterval(() => this.loadCommentsFromServer(), this.props.pollInterval)
 	}
 	render(){
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
 				<CommentList data={this.state.data}/>
-				<CommentForm />
+				<CommentForm onCommentSubmit={this.handleCommentSubmit} />
 			</div>
 		);
 	}
